f4ed47839b9d9e3cc3fae819ac10391858146260
whatsmyname0 <mikepham2007@gmail.com>
2024-09-13 10:12:58 -0700

    Final

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,17 @@
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        double num = 51.2731;
+        System.out.println("Decimal number: " + num);
+        num *= 1000;
+        int num1 = (int) (num % 10);
+        num /= 10;
+        int num2 = (int) (num % 10);
+        num /= 10;
+        int num3 =(int) (num % 10);
+        num /= 10;
+        System.out.print("Answer: " + num3 + " " + num2 + " " + num1 + " ");
+    }
+}
 
